dab8811496a3874eb1cdfe63f78287af31bdd5b1
김성식 <colton.kim@daumkakao.com>
2020-04-14 20:54:16 +0900

    loading

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  const [loaded, setLoaded] = useState(false);
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+
+  const loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font,
+    });
+  };
+
+  if (loaded) {
+    return (
+      <View style={styles.container}>
+        <Text> Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  } else {
+    return (
+      <AppLoading
+        onError={handleError}
+        onFinish={() => setLoaded(true)}
+        startAsync={loadAssets}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
